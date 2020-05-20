3e6df5d9126a928ccf1e441b3032c7b9dc84ebd6
mkallgren08 <mkallgren08@gmail.com>
2020-05-20 13:19:02 -0400

    started learning java!

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+  public static void main(String[] args){
+    String msg = "Hello World";
+    System.out.println("\n-------------------------\n"+ 
+    msg + "\n-------------------------");
+  };
+};
